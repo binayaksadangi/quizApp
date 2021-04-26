28f486af03c2d1c679b046393db030ed7d7350c5
Binayak <56728301+binayaksadangi@users.noreply.github.com>
2021-04-26 12:18:50 +0530

    created

---
+++ script.js
@@ -0,0 +1,32 @@
+const quizData = [
+    {
+        question: 'Best team in IPL?',
+        a: 'MI',
+        b: 'CSK',
+        c: 'RCB',
+        d: 'DC',
+        correct: 'a'
+    },{
+        question: 'Most used Programming language in 2020?',
+        a: 'Python',
+        b: 'JavaScript',
+        c: 'java',
+        d: 'c++',
+        correct: 'b'
+    },{
+        question: 'Who is the PM of India',
+        a: 'Mukesh Ambani',
+        b: 'Imran Tahir',
+        c: 'Narendra Modi',
+        d: 'Manas Ranjan Kabat',
+        correct: 'c'
+    },{
+        question: 'The full form of popular language JS?',
+        a: 'Jay Sambhoo',
+        b: 'JavaScript',
+        c: 'Joint Supplier',
+        d: 'Java Supplier',
+        correct: 'b'
+    }
+]
+
